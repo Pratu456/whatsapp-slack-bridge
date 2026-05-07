5d7121a467a3ef562c64c96f47751e681b5a8fe4
Pratu456 <deshmukhpratiksha456@gmail.com>
2026-05-08 00:18:15 +0530

    fix: use Gmail SMTP port 587 with TLS

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,11 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 const sendEmail = async ({ to, subject, html }) => {
